b00524ddbfd078dd1573a1e4d8d7bb69c82eca57
goral202 <jakub144202@wp.pl>
2024-12-20 11:44:04 +0100

    webapp

--- app_ros.py
+++ app_ros.py
@@ -1,10 +1,16 @@
 from flask import Flask, Response, render_template
 import cv2
 import threading
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from nav_msgs.msg import OccupancyGrid
+import numpy as np
 
 app = Flask(__name__)
 current_frame_camera = None
 current_frame_map = None
+bridge = CvBridge() 
 
 @app.route('/camera_feed')
 def video_feed_camera():
@@ -34,15 +40,37 @@ def index():
 
 def update_camera_frame():
     global current_frame_camera
-    while True:
-        current_frame_camera = cv2.imread('camera_image.jpg') 
+    def image_callback(msg):
+        global current_frame_camera
+        try:
+            current_frame_camera = bridge.imgmsg_to_cv2(msg, "bgr8")
+        except Exception as e:
+            print("Error converting image from ROS:", e)
+
+    rospy.Subscriber("/camera/image_raw", Image, image_callback)
+    rospy.spin()  
 
 def update_map_frame():
     global current_frame_map
-    while True:
-        current_frame_map = cv2.imread('map_image.jpg')
+    def map_callback(msg):
+        global current_frame_map
+        try:
+            map_data = np.array(msg.data, dtype=np.int8)
+            width = msg.info.width
+            height = msg.info.height
+
+            map_image = map_data.reshape((height, width))
+
+            current_frame_map = cv2.convertScaleAbs(map_image, alpha=255) 
+        except Exception as e:
+            print("Error converting map from ROS:", e)
+
+    rospy.Subscriber("/map", OccupancyGrid, map_callback)
+    rospy.spin() 
 
 if __name__ == '__main__':
+    rospy.init_node('flask_ros_node', anonymous=True)
+
     threading.Thread(target=update_camera_frame, daemon=True).start()
     threading.Thread(target=update_map_frame, daemon=True).start()
 
